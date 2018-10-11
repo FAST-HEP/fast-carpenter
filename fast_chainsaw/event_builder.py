90e927532e50d291f2ae92dafc965eb48febec5f
Ben Krikler <bek07@ic.ac.uk>
2018-10-11 19:19:31 +0200

    Add the event_builder file based on Shane's EventBuilder file in atuproot

---
+++ fast_chainsaw/event_builder.py
@@ -0,0 +1,37 @@
+import uproot
+
+from atuproot.BEvents import BEvents
+from .masked_tree import MaskedUprootTree
+
+class EventBuilder(object):
+    def __init__(self, config):
+        self.config = config
+
+    def __repr__(self):
+        return '{}({!r})'.format(
+            self.__class__.__name__,
+            self.config,
+        )
+
+    def __call__(self):
+        if len(self.config.inputPaths) != 1:
+            # TODO - support multiple inputPaths
+            raise AttributeError("Multiple inputPaths not yet supported")
+
+        # Try to open the tree - some machines have configured limitations
+        # which prevent memmaps from begin created. Use a fallback - the
+        # localsource option
+        try:
+            rootfile = uproot.open(self.config.inputPaths[0])
+            tree = rootfile[self.config.treeName]
+        except:
+            rootfile = uproot.open(self.config.inputPaths[0],
+                               localsource = uproot.FileSource.defaults)
+            tree = rootfile [self.config.treeName]
+
+        events = BEvents(MaskedUprootTree(tree),
+                         self.config.nevents_per_block,
+                         self.config.start_block,
+                         self.config.stop_block)
+        events.config = self.config
+        return events
