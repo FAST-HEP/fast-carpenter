13b516dab7bf43cbfdc574a850416036e65e7a02
Ben Krikler <b.krikler@cern.ch>
2018-11-13 16:49:35 +0000

    Refactor and add add index method for reductions

--- fast_carpenter/define/reductions.py
+++ fast_carpenter/define/reductions.py
@@ -1,11 +1,35 @@
-from numpy import count_nonzero
+import numpy as np
+import six
 
 
-__all__ = ["sum", "count_nonzero", "max", "min", "argmax", "argmin"]
+__all__ = ["get_pandas_reduction"]
 
 
-sum = "sum"
-max = "max"
-min = "min"
-argmax = "argmax"
-argmin = "argmin"
+class BadReductionConfig(Exception):
+    pass
+
+
+_pandas_aggregates = ["sum", "max", "min", "argmax", "argmin"]
+_numpy_ops = ["count_zero"]
+
+
+def get_pandas_reduction(stage_name, reduction):
+    if not isinstance(reduction, six.string_types):
+        msg = "{}: requested reduce method is not a string"
+        raise BadReductionConfig(msg.format(stage_name))
+
+    if reduction in _pandas_aggregates:
+        return lambda groups: groups.agg(reduction)
+    elif reduction in _numpy_ops:
+        op = getattr(np, reduction)
+        return lambda groups: groups.agg(op)
+    else:
+        try:
+            index = int(reduction)
+        except ValueError:
+            pass
+        else:
+            return lambda groups: groups.nth(index)
+
+    msg = "{}: Unknown method to reduce: '{}'"
+    raise BadReductionConfig(msg.format(stage_name, reduction))
