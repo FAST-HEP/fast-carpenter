6fe883e0a842510528fad48baf5f6b38ce55d960
Ben Krikler <b.krikler@cern.ch>
2018-11-15 13:01:29 +0000

    bump version again, messed up tagging...

--- setup.py
+++ setup.py
@@ -53,6 +53,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/fast-hep/public/fast-carpenter',
-    version='0.5.1',
+    version='0.5.2',
     zip_safe=True,
 )
