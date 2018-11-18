57e9204448581c66f5c9ea2b8a1fafaa291540d9
Ben Krikler <b.krikler@cern.ch>
2018-11-18 01:35:10 +0000

    Increment patch release number

--- setup.py
+++ setup.py
@@ -53,6 +53,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/fast-hep/public/fast-carpenter',
-    version='0.5.3',
+    version='0.5.4',
     zip_safe=True,
 )
