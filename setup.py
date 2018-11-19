777ed8790a4e2540fce91e92b148db93aa81a1bb
Ben Krikler <b.krikler@cern.ch>
2018-11-19 13:47:59 +0000

    Bump patch version number

--- setup.py
+++ setup.py
@@ -53,6 +53,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://gitlab.cern.ch/fast-hep/public/fast-carpenter',
-    version='0.5.4',
+    version='0.5.5',
     zip_safe=True,
 )
