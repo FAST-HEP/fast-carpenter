03737352b976641355da5c0fc96d9d7575cf8e64
Ben Krikler <bek07@ic.ac.uk>
2018-11-27 10:26:09 -0700

    Make sure we use uproot >= 3

--- setup.py
+++ setup.py
@@ -11,7 +11,8 @@ with open('README.md') as readme_file:
 # with open('HISTORY.rst') as history_file:
 #     history = history_file.read()
 
-requirements = ['atuproot', 'fast-flow', 'fast-curator', 'awkward', 'pandas', 'numpy', 'numba', 'atsge', 'numexpr']
+requirements = ['atuproot', 'fast-flow', 'fast-curator', 'awkward',
+                'pandas', 'numpy', 'numba', 'atsge', 'numexpr', 'uproot>=3']
 repositories = []
 
 setup_requirements = ['pytest-runner', ]
