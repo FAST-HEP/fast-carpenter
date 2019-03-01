80609bddea82ba096dec3961d053bdfd70fd8b16
Ben Krikler <bek07@ic.ac.uk>
2019-03-02 00:07:24 +0100

    Revert "Bump version: 0.6.7 → 0.6.8"
    This reverts commit 4e024593b2c4198b3f70fc961a2cb594dc898396.

--- setup.py
+++ setup.py
@@ -54,6 +54,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://gitlab.cern.ch/fast-hep/public/fast-carpenter',
-    version='0.6.8',
+    version='0.6.7',
     zip_safe=True,
 )
