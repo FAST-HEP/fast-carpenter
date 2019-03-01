b77dc8a49f0233cde676330e2f040a6a8c3598bb
Ben Krikler <bek07@ic.ac.uk>
2019-03-02 00:05:32 +0100

    Improve unit tests

--- tests/test_expressions.py
+++ tests/test_expressions.py
@@ -1,12 +1,4 @@
-import pytest
-import uproot
 from fast_carpenter import expressions
-
-
-@pytest.fixture
-def infile():
-    filename = "tests/data/CMS_HEP_tutorial_ww.root"
-    return uproot.open(filename)["events"]
 
 
 def test_get_branches(infile):
@@ -19,3 +11,15 @@ def test_get_branches(infile):
     cut = "NMuon_not_found > 1 and NElectron > 3"
     branches = expressions.get_branches(cut, valid)
     assert branches == ["NElectron"]
+
+
+def test_evaluate(wrapped_tree):
+    Muon_py, Muon_pz = wrapped_tree.arrays(["Muon_Py", "Muon_Pz"], outputtype=tuple)
+    mu_pt = expressions.evaluate(wrapped_tree, "sqrt(Muon_Px**2 + Muon_Py**2)")
+    assert len(mu_pt) == 100
+    assert all(mu_pt.counts == Muon_py.counts)
+
+
+def test_evaluate_bool(wrapped_tree):
+    all_true = expressions.evaluate(wrapped_tree, "Muon_Px == Muon_Px")
+    assert all(all_true.all())
