c9625dc5cb7aa32e46e34e28c96b15cb046975f8
Ben Krikler <bek07@ic.ac.uk>
2019-03-08 10:48:30 +0100

    Bump version: 0.7.0 → 0.7.1

--- setup.py
+++ setup.py
@@ -55,6 +55,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://gitlab.cern.ch/fast-hep/public/fast-carpenter',
-    version='0.7.0',
+    version='0.7.1',
     zip_safe=True,
 )
