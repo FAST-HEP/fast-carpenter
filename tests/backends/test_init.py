3921f3f16151d3825e7160bb6afa256ea4088fcf
Ben Krikler <bek07@ic.ac.uk>
2019-11-01 05:44:08 +0100

    Update unit test

--- tests/backends/test_init.py
+++ tests/backends/test_init.py
@@ -2,5 +2,5 @@ import fast_carpenter.backends as backends
 
 
 def test_get_backend():
-    alphatwirl_back = backends.get_backend("alphatwirl")
+    alphatwirl_back = backends.get_backend("alphatwirl:multiprocessing")
     assert hasattr(alphatwirl_back, "execute")
