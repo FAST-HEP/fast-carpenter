1948cd8d874d6325bd01fe89fcf21088cf9ddd25
Ben Krikler <bek07@ic.ac.uk>
2019-11-01 05:45:21 +0100

    Extend unit tests

--- tests/backends/test_init.py
+++ tests/backends/test_init.py
@@ -1,6 +1,11 @@
+import pytest
 import fast_carpenter.backends as backends
 
 
 def test_get_backend():
     alphatwirl_back = backends.get_backend("alphatwirl:multiprocessing")
     assert hasattr(alphatwirl_back, "execute")
+
+    with pytest.raises(ValueError) as e:
+        backends.get_backend("doesn't exist")
+    assert "Unknown backend" in str(e)
