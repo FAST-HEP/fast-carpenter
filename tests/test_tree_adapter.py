436a38ef387f624d65958ad42de8cf227ec51efe
Luke Kreczko <lkreczko@googlemail.com>
2022-01-07 14:11:32 +0000

    added Tree Adaptor tests for uproot4

--- tests/test_tree_adapter.py
+++ tests/test_tree_adapter.py
@@ -1,14 +1,31 @@
 from fast_carpenter.tree_adapter import TreeToDictAdaptorV0, TreeToDictAdaptorV1
-
-
-def test_uproot3_access(uproot3_tree):
-    adapter = TreeToDictAdaptorV0(uproot3_tree)
-    assert adapter.num_entries == uproot3_tree.numentries
-
+import awkward as ak
 
 ###############################################################################
 # Uproot4 tests
 ###############################################################################
-def test_uproot4_access(uproot4_tree):
+
+
+def test_uproot3_num_entries(uproot3_tree):
+    adapter = TreeToDictAdaptorV0(uproot3_tree)
+    assert adapter.num_entries == uproot3_tree.numentries
+
+
+def test_uproot3_getitem(uproot3_tree):
+    adapter = TreeToDictAdaptorV0(uproot3_tree)
+    assert ak.all(adapter["Muon_Py"] == uproot3_tree["Muon_Py"].array())
+
+###############################################################################
+# Uproot4 tests
+###############################################################################
+
+
+def test_uproot4_num_entries(uproot4_tree):
     adapter = TreeToDictAdaptorV1(uproot4_tree)
     assert adapter.num_entries == uproot4_tree.num_entries
+
+
+def test_uproot4_getitem(uproot4_tree):
+    adapter = TreeToDictAdaptorV1(uproot4_tree)
+    print(uproot4_tree, type(uproot4_tree), dir(uproot4_tree))
+    assert ak.all(adapter["Muon_Py"] == uproot4_tree["Muon_Py"].array())
