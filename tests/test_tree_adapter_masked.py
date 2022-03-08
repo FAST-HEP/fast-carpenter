4a2387ab13942bbfdad35a9f5cac9a9972cd53f1
Luke Kreczko <lkreczko@googlemail.com>
2022-03-08 16:42:34 +0000

    added specific test for a complex cut/selection

--- tests/test_tree_adapter_masked.py
+++ tests/test_tree_adapter_masked.py
@@ -32,6 +32,19 @@ def test_single_mask_twice(fake_data_events, full_wrapped_tree, at_least_one_muo
     assert ak.all(fake_data_events.tree["Muon_Pz"] == ak.mask(full_wrapped_tree["Muon_Pz"], mask))
 
 
+def test_complex_mask(fake_data_events, full_wrapped_tree, at_least_one_muon_plus, tmpdir):
+    full_size = len(fake_data_events)
+    check_data(fake_data_events, n_data=full_size, n_nonzero=full_size, n_mask=full_size)
+
+    at_least_one_muon_plus.event(fake_data_events)
+
+    check_data(fake_data_events, n_data=full_size, n_nonzero=2, n_mask=full_size)
+
+    mask = fake_data_events.tree._mask
+    assert ak.count_nonzero(mask) == 2
+    assert ak.all(fake_data_events.tree["Muon_Pz"] == ak.mask(full_wrapped_tree["Muon_Pz"], mask))
+
+
 def test_multi_mask(fake_data_events, full_wrapped_tree, at_least_one_muon, at_least_one_muon_plus, tmpdir):
     full_size = len(fake_data_events)
     check_data(fake_data_events, n_data=full_size, n_nonzero=full_size, n_mask=full_size)
