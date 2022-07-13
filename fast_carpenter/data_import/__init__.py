9b5ba9b8477f2237334ea82ca0bb875e807512e8
Luke Kreczko <lkreczko@googlemail.com>
2022-07-13 21:37:34 +0100

    data_import_plugins: added MultiTreeImport

--- fast_carpenter/data_import/__init__.py
+++ fast_carpenter/data_import/__init__.py
@@ -1,12 +1,17 @@
 from pathlib import Path
 from typing import Optional
+
+import fast_curator
+
 from ._base import DataImportBase
-from ._uproot4 import Uproot4DataImport
+from ._multitree import MultiTreeImport
 from ._uproot3 import Uproot3DataImport
+from ._uproot4 import Uproot4DataImport
 
 _DATA_IMPORT_PLUGINS = {
     "uproot4": Uproot4DataImport,
     "uproot3": Uproot3DataImport,
+    "multitree": MultiTreeImport,
 }
 
 
@@ -17,22 +22,29 @@ def register_data_import_plugin(plugin_name: str, plugin_class: DataImportBase) 
     _DATA_IMPORT_PLUGINS[plugin_name] = plugin_class
 
 
-def _process_plugin_config(plugin_name: str, plugin_config: Optional[Path] = None) -> None:
+def _process_plugin_config(
+    plugin_name: str, plugin_config: Optional[str] = None
+) -> None:
     """
-        Process the plugin configuration file.
-        Reads the "register" and "plugin_name" sections to register and configure the plugin.
+    Process the plugin configuration file.
+    Reads the "register" and "plugin_name" sections to register and configure the plugin.
     """
     if plugin_config is None:
         return
+    plugin_config = Path(plugin_config)
     if not plugin_config.exists():
         raise ValueError(f"Plugin config file {plugin_config} does not exist")
     if not plugin_config.is_file():
         raise ValueError(f"Plugin config file {plugin_config} is not a file")
 
+    return fast_curator.read.from_yaml(plugin_config)
+
     raise NotImplementedError("Plugin config file processing not implemented yet")
 
 
-def get_data_import_plugin(plugin_name: str, plugin_config: Optional[Path] = None) -> DataImportBase:
+def get_data_import_plugin(
+    plugin_name: str, plugin_config: Optional[Path] = None
+) -> DataImportBase:
     """
     Get a data import plugin by name.
     """
---
+++ fast_carpenter/data_import/_multitree.py
@@ -0,0 +1,42 @@
+from ..data_mapping import create_mapping
+from ._base import DataImportBase
+
+
+class MultiTreeImport(DataImportBase):
+    def __init__(self, config):
+        super().__init__(config)
+        self._process_config()
+
+    def _process_config(self):
+        pass
+
+    def open(self, paths):
+        import uproot
+
+        if len(paths) != 1:
+            # TODO - support multiple paths
+            raise AttributeError("Multiple paths not yet supported")
+        # Try to open the tree - some machines have configured limitations
+        # which prevent memmaps from begin created. Use a fallback - the
+        # localsource option
+        input_file = paths[0]
+        try:
+            rootfile = uproot.open(input_file)
+        except MemoryError:
+            rootfile = uproot.open(input_file, file_handler=uproot.source.chunk.Source)
+
+        trees = []
+        if "treename" in self.config:
+            if isinstance(self.config["treename"], str):
+                trees.append(self.config["treename"])
+            else:
+                trees = self.config["treename"]
+
+        data_mapping = create_mapping(
+            input_file=rootfile,
+            treenames=trees,
+            connector="file",
+            methods="uproot4",
+        )
+
+        return data_mapping
