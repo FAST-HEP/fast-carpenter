351d1f06fd43b58cd5fa6abc818b97196342db07
kreczko <lkreczko@googlemail.com>
2022-07-29 16:29:41 +0100

    added Dask backend (draft)

--- fast_carpenter/backends/__init__.py
+++ fast_carpenter/backends/__init__.py
@@ -8,17 +8,28 @@ from functools import partial
 
 def get_alphatwirl():
     from . import _alphatwirl
+
     return _alphatwirl
 
 
 def get_coffea():
     from . import coffea
+
     return coffea
 
 
 def get_parsl(processing_mode):
     from ._parsl import ParslBackend
+
     backend = ParslBackend()
+    backend.configure(processing_mode=processing_mode)
+    return backend
+
+
+def get_dask(processing_mode):
+    from ._dask import DaskBackend
+
+    backend = DaskBackend()
     backend.configure(processing_mode=processing_mode)
     return backend
 
@@ -30,6 +41,10 @@ KNOWN_BACKENDS = {
     "coffea:local": get_coffea,
     "coffea:parsl": get_coffea,
     "coffea:dask": get_coffea,
+    "dask:local": partial(get_dask, processing_mode="local"),
+    "dask:htcondor": partial(get_dask, processing_mode="htcondor"),
+    "dask:sge": partial(get_dask, processing_mode="sge"),
+    "dask:dirac": partial(get_dask, processing_mode="dirac"),
     "parsl:local": partial(get_parsl, "local"),
     "parsl:htcondor": partial(get_parsl, "local"),
     "parsl:dirac": partial(get_parsl, "local"),
@@ -40,5 +55,7 @@ KNOW_BACKENDS_NAMES = ", ".join(list(KNOWN_BACKENDS.keys()))
 
 def get_backend(name):
     if name not in KNOWN_BACKENDS:
-        raise ValueError(f"Unknown backend requested, '{name}'. Known backends: {KNOW_BACKENDS_NAMES}")
+        raise ValueError(
+            f"Unknown backend requested, '{name}'. Known backends: {KNOW_BACKENDS_NAMES}"
+        )
     return KNOWN_BACKENDS[name]()
---
+++ fast_carpenter/backends/_dask.py
@@ -0,0 +1,49 @@
+from dask.distributed import Client
+from dask.multiprocessing import get
+
+from ._base import InputData, ProcessingBackend, Workflow
+
+
+def configure_dask(client, **kwargs):
+
+    kwargs.setdefault("processes", False)
+    kwargs.setdefault("threads_per_worker", 2)
+    kwargs.setdefault("n_workers", 2)
+    kwargs.setdefault("memory_limit", "1GB")
+
+    if client:
+        return Client(client)
+    else:
+        return Client(**kwargs)
+
+
+class DaskBackend(ProcessingBackend):
+    def __init__(self):
+        print("Using experimental Dask backend")
+        print("There be dragons")
+
+    def configure(self, **kwargs):
+        # set sensible defaults
+        self._config.update(kwargs)
+
+    def execute(self, sequence, datasets, args, plugins):
+        dask_client = configure_dask(client=None)
+        # sequence is a list of steps, we need a dict of steps
+        sequence = {s.name: s for s in sequence}
+        workflow = Workflow(sequence)
+        workflow.to_image("carpenter.png")
+
+        input_data = InputData(datasets)
+        data_import_plugin = plugins.get("data_import")
+        workflow = workflow.add_data_stage(data_import_plugin, input_data)
+        workflow.to_image("carpenter-with-data-import.png")
+        workflow.add_collector()
+        workflow.to_image("carpenter-with-data-import-and-collector.png")
+
+        # delayed_dsk = Delayed("w", workflow.task_graph)
+        # dask.compute(delayed_dsk)
+
+        get(workflow.task_graph, "__reduce__")
+        # workflow.final_task().result()
+        dask_client.close()
+        return (0, 0)
